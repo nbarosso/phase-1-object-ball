ef3fb8b8a51da306a040f7ef4d2123b0cc60781c
Nicholas Barosso <nbarosso@gmail.com>
2023-08-19 08:34:00 -0500

    incomplete lab - go back and finish

---
+++ src/00-objectball.js
@@ -0,0 +1,32 @@
+
+
+
+const teams = {
+    home: {
+        teamName: 'Brooklyn Nets',
+        colors: ['Black', 'White'],
+        players: [
+            {   name: 'Alan Anderson',
+                number: 0,
+                shoe: 16,
+                points: 22,
+                rebounds: 12,
+                assists: 12,
+                steals: 3,
+                blocks: 1,
+                slamDunks: 1,
+        }
+            {   name: 'Reggie Evans',
+                number: 30,
+                shoe: 14,
+                points: 12,
+                rebounds: 12,
+                assists: 12,
+                steals: 12,
+                blocks: 12,
+                slamDunks: 7,
+        }
+    }
+    away: {
+        teamName: 'Charlotte Hornets',
+        colors: ['Turquoise', 'Purple'],
